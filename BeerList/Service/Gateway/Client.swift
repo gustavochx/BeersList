45784eb1c7d731132db95c3897fe22aba57088eb
Gustavo <gustavochx@gmail.com>
2019-06-12 11:45:10 -0300

    Removing unused methods from client

--- BeerList/Service/Gateway/Client.swift
+++ BeerList/Service/Gateway/Client.swift
@@ -72,28 +72,6 @@ extension Client {
         task.resume()
     }
 
-    func fetchMultipart<T:Decodable> (using request: URLRequest, images: [UIImage],
-                                      imagesNames: [String], otherDocument: String?,  decode: @escaping(Decodable) -> T?, completionHandler: @escaping(Result<T,APIError>) -> Void) {
-
-        let task = multipartTask(request: request, images: images, imageNames: imagesNames, otherParameter: otherDocument, decodingType: T.self, completionHandler: { json, error in
-
-            guard let json = json else {
-                if let error = error {
-                    completionHandler(Result.failure(error))
-                } else {
-                    completionHandler(Result.failure(.invalidData))
-                }
-                return
-            }
-
-            if let data = decode(json) {
-                completionHandler(Result.success(data))
-            } else {
-                completionHandler(Result.failure(.jsonParsingFailure))
-            }
-        })
-        task.resume()
-    }
 
 
     func decodingTask <T:Decodable>
@@ -151,79 +129,6 @@ extension Client {
         return task
     }
 
-    func multipartTask <T:Decodable> (request: URLRequest,
-                                      images: [UIImage],
-                                      imageNames: [String],
-                                      otherParameter: String?,
-                                      decodingType : T.Type,
-                                      completionHandler completion: @escaping JSONTaskCompletionHandler) -> URLSessionDataTask {
 
-        // generate boundary string using a unique per-app string
-        let boundary = UUID().uuidString
-        var data = Data()
-
-        // Set Content-Type Header to multipart/form-data, this is equivalent to submitting form data with file upload in a web browser
-        // And the boundary is also set here
-        var urlRequest = request
-        urlRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-
-
-        // Add the reqtype field and its value to the raw http request data
-        let fieldName = "reqtype"
-        let fieldValue = "fileupload"
-
-        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
-        data.append("Content-Disposition: form-data; name=\"\(fieldName)\"\r\n\r\n".data(using: .utf8)!)
-        data.append("\(fieldValue)".data(using: .utf8)!)
-
-        if(otherParameter != nil) {
-            let fieldName2 = "document"
-            let fieldValue2 = otherParameter!
-
-            data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
-            data.append("Content-Disposition: form-data; name=\"\(fieldName2)\"\r\n\r\n".data(using: .utf8)!)
-            data.append("\(fieldValue2)".data(using: .utf8)!)
-        }
-
-        for i in 0..<imageNames.count {
-            let filename = imageNames[i]
-
-            // Add the image data to the raw http request data
-            data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
-            data.append("Content-Disposition: form-data; name=\"\(filename)\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
-            data.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
-            data.append(images[i].jpegData(compressionQuality: 0.7)!)
-            data.append("\r\n".data(using: .utf8)!)
-        }
-
-        data.append("--\(boundary)--\r\n".data(using: .utf8)!)
-
-        // Send a POST request to the URL, with the data we created earlier
-        let task = session.uploadTask(with: urlRequest, from: data, completionHandler: { data, response, error in
-
-            guard let httpResponse = response as? HTTPURLResponse else {
-                completion(nil, .requestFailed)
-                return
-            }
-            if httpResponse.statusCode == 200 || httpResponse.statusCode == 201 {
-                if let data = data {
-                    do{
-                        let genericModel = try JSONDecoder().decode(decodingType, from: data)
-                        completion(genericModel,nil)
-
-                    } catch {
-
-                        completion(nil, .jsonParsingFailure)
-
-                    }
-                } else {
-                    completion(nil, .invalidData)
-                }
-            } else {
-                completion(nil, .responseUnsuccessful)
-            }
-        })
-        return task
-    }
 
 }
